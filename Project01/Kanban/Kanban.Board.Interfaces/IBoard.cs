ce4e2029139125d76d3d27fccc68722cbc257973
Soderberg Zuo <zuohd@shinetechchina.com>
2016-11-08 14:00:10 +0800

    Implement board actor and remove useless api controller

--- Project01/Kanban/Kanban.Board.Interfaces/IBoard.cs
+++ Project01/Kanban/Kanban.Board.Interfaces/IBoard.cs
@@ -13,17 +13,7 @@ namespace Kanban.Board.Interfaces
     /// </summary>
     public interface IBoard : IActor
     {
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <returns></returns>
-        Task<int> GetCountAsync();
-
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        Task SetCountAsync(int count);
+        Task Initialise(string name);
+        Task<string> Details();
     }
 }
--- Project01/Kanban/Kanban.Board/Board.cs
+++ Project01/Kanban/Kanban.Board/Board.cs
@@ -10,62 +10,29 @@ using Kanban.Board.Interfaces;
 
 namespace Kanban.Board
 {
-    /// <remarks>
-    /// This class represents an actor.
-    /// Every ActorID maps to an instance of this class.
-    /// The StatePersistence attribute determines persistence and replication of actor state:
-    ///  - Persisted: State is written to disk and replicated.
-    ///  - Volatile: State is kept in memory only and replicated.
-    ///  - None: State is kept in memory only and not replicated.
-    /// </remarks>
     [StatePersistence(StatePersistence.Persisted)]
     internal class Board : Actor, IBoard
     {
-        /// <summary>
-        /// Initializes a new instance of Board
-        /// </summary>
-        /// <param name="actorService">The Microsoft.ServiceFabric.Actors.Runtime.ActorService that will host this actor instance.</param>
-        /// <param name="actorId">The Microsoft.ServiceFabric.Actors.ActorId for this actor instance.</param>
         public Board(ActorService actorService, ActorId actorId)
             : base(actorService, actorId)
         {
         }
-
-        /// <summary>
-        /// This method is called whenever an actor is activated.
-        /// An actor is activated the first time any of its methods are invoked.
-        /// </summary>
+      
         protected override Task OnActivateAsync()
         {
             ActorEventSource.Current.ActorMessage(this, "Actor activated.");
-
-            // The StateManager is this actor's private state store.
-            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
-            // Any serializable object can be saved in the StateManager.
-            // For more information, see https://aka.ms/servicefabricactorsstateserialization
-
             return this.StateManager.TryAddStateAsync("count", 0);
         }
 
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <returns></returns>
-        Task<int> IBoard.GetCountAsync()
+
+        Task IBoard.Initialise(string name)
         {
-            return this.StateManager.GetStateAsync<int>("count");
+            return this.StateManager.SetStateAsync("Name", name);
         }
 
-        /// <summary>
-        /// TODO: Replace with your own actor method.
-        /// </summary>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        Task IBoard.SetCountAsync(int count)
+        Task<string> IBoard.Details()
         {
-            // Requests are not guaranteed to be processed in order nor at most once.
-            // The update function here verifies that the incoming count is greater than the current count to preserve order.
-            return this.StateManager.AddOrUpdateStateAsync("count", count, (key, value) => count > value ? count : value);
+            return StateManager.GetStateAsync<string>("Name");
         }
     }
 }

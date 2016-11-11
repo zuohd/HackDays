a5cc2a7d72a2a94c5ceafc30696330bcc5ced9bb
Soderberg Zuo <zuohd@shinetechchina.com>
2016-11-11 17:21:12 +0800

    Add vote apicontroller

--- Project02/Voting/VotingService/Controllers/VotesController.cs
+++ Project02/Voting/VotingService/Controllers/VotesController.cs
@@ -1,4 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
 using System.Web.Http;
 
 namespace VotingService.Controllers
@@ -6,31 +11,75 @@ namespace VotingService.Controllers
     [ServiceRequestActionFilter]
     public class VotesController : ApiController
     {
-        // GET api/values 
-        public IEnumerable<string> Get()
+        public static long _requestCount = 0L;
+        private static Dictionary<string, int> _counts = new Dictionary<string, int>();
+
+        [HttpGet]
+        [Route("api/votes")]
+        public HttpResponseMessage Get()
         {
-            return new string[] { "value1", "value2" };
+            Interlocked.Increment(ref _requestCount);
+            List<KeyValuePair<string, int>> votes = new List<KeyValuePair<string, int>>(_counts.Count);
+            foreach (var kvp in _counts)
+            {
+                votes.Add(kvp);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK, votes);
+            response.Headers.CacheControl = new CacheControlHeaderValue() {NoCache = true, MustRevalidate = true};
+            return response;
         }
 
-        // GET api/values/5 
-        public string Get(int id)
+        [HttpPost]
+        [Route("api/{key}")]
+        public HttpResponseMessage Post(string key)
         {
-            return "value";
+            Interlocked.Increment(ref _requestCount);
+            if (!_counts.ContainsKey(key))
+            {
+                _counts.Add(key, 1);
+            }
+            else
+            {
+                _counts[key] += 1;
+            }
+            return Request.CreateResponse(HttpStatusCode.NoContent);
         }
 
-        // POST api/values 
-        public void Post([FromBody]string value)
+        [HttpGet]
+        [Route("api/{key}")]
+        public HttpResponseMessage Delete(string key)
         {
+            Interlocked.Increment(ref _requestCount);
+            if (_counts.ContainsKey(key))
+            {
+                if (_counts.Remove(key))
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
         }
 
-        // PUT api/values/5 
-        public void Put(int id, [FromBody]string value)
+        [HttpGet]
+        [Route("api/{file}")]
+        public HttpResponseMessage GetFile(string file)
         {
-        }
 
-        // DELETE api/values/5 
-        public void Delete(int id)
-        {
+            string response = null;
+            string responseType = "text/html";
+            Interlocked.Increment(ref _requestCount);
+            if (file == "index.html")
+            {
+                var path = $@"..\votingServicePkg.code.1.0.0\{file}";
+                response = File.ReadAllText(path);
+            }
+            if (response != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, response, responseType);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "file");
+            }
         }
     }
-}

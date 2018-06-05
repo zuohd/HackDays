e87c28d22c17d730d5923e41398a9352b3cb1dd5
Soderberg Zuo <zuo.houde@gmail.com>
2018-06-05 18:29:30 +0800

    Add component

--- Project03/first-react-app/my-app/src/index.js
+++ Project03/first-react-app/my-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import ClickCounter from './ClickCounter';
 import registerServiceWorker from './registerServiceWorker';
-
-var element=React.createElement('h1',{className:'greetings'},'Hello, Soderberg!');
-ReactDOM.render(element, document.getElementById('root'));
+//#var element=React.createElement('h1',{className:'greetings'},'Hello, Soderberg!');
+ReactDOM.render(<ClickCounter />, document.getElementById('root'));
 registerServiceWorker();

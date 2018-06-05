bf14eacd559931c3fa9d8ea521ec350b023df2b9
Soderberg Zuo <zuo.houde@gmail.com>
2018-06-05 19:37:02 +0800

    add counter component

---
+++ Project03/first-react-app/my-app/src/Counter.js
@@ -0,0 +1,35 @@
+import React , { Component } from 'react';
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+        //this.onClickIncrementButton=this.onClickIncrementButton.bind(this);
+        this.onClickIncrementButton = this.onClickIncrementButton.bind(this);
+        this.onClickDecrementButton = this.onClickDecrementButton.bind(this);
+        this.state={
+            count:props.initValue||0
+        }
+    }
+    onClickDecrementButton(){
+        this.setState({count:this.state.count-1});
+    }
+    onClickIncrementButton(){
+        this.setState({count:this.state.count+1});
+    }
+
+    render() {
+        const {caption}=this.props;
+        const buttonStyle = {
+            margin: '10px'
+          };
+        return (
+            <div>
+                <button style={buttonStyle} onClick={this.onClickIncrementButton}>+</button>
+                <button style={buttonStyle} onClick={this.onClickDecrementButton}>-</button>
+                <span>{caption} count:{this.state.count}</span>
+            </div>
+        );
+    }    
+  
+}
+export default Counter;
--- Project03/first-react-app/my-app/src/index.js
+++ Project03/first-react-app/my-app/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import ClickCounter from './ClickCounter';
+//import ClickCounter from './ClickCounter';
+import ControlPanel from './ControlPanel';
 import registerServiceWorker from './registerServiceWorker';
 //#var element=React.createElement('h1',{className:'greetings'},'Hello, Soderberg!');
-ReactDOM.render(<ClickCounter />, document.getElementById('root'));
+//ReactDOM.render(<ClickCounter />, document.getElementById('root'));
+ReactDOM.render(<ControlPanel />, document.getElementById('root'));
+
 registerServiceWorker();

1c8da2668bf06d6245a2980101f7c303d115da2b
Soderberg Zuo <zuo.houde@gmail.com>
2018-06-19 15:39:57 +0800

    Add app dispatcher

---
+++ Project03/first-react-app/my-app/src/Actions.js
@@ -0,0 +1,18 @@
+import * as ActionTypes from './ActionTypes.js';
+import AppDispatcher from './AppDispatcher';
+
+export const increment=(counterCaption)=>{
+    AppDispatcher.dispatch({
+        type:ActionTypes.INCREMENT,
+        counterCaption:counterCaption
+    }
+
+    );
+}
+
+export const decrement=(counterCaption)=>{
+    AppDispatcher.dispatch({
+        type:ActionTypes.DECREMENT,
+        counterCaption:counterCaption
+    });
+}

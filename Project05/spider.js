20f30a0a523dbe67dc94cfc4fe39022c25c31902
Soderberg Zuo <zuo.houde@gmail.com>
2018-10-13 18:08:14 +0800

    import cheerio package

--- Project05/spider.js
+++ Project05/spider.js
@@ -1,6 +1,39 @@
 var http=require("http")
 var https=require("https")
+var cheerio=require('cheerio')
 var url='https://www.lagou.com/'
+
+function filterMenu(html){
+    var $=cheerio.load(html)
+    var menu=$('.menu_main')
+    var menuData=[]
+    menu.each(function(index,value){
+        var menuTitle=$(value).find('h2').text()
+        var menuLists=$(value).find('a')
+        var menuList=[];
+        
+        menuLists.each(function(index,value){
+            menuList.push($(value).text())
+        })
+
+        menuData.push({
+            menuTitle:menuTitle,
+            menuList:menuList
+
+        })
+    })
+    return menuData;
+}
+
+function printMenu(menu){
+    menu.forEach(function(value){
+        console.log(value.menuTitle+'\n')
+        value.menuList.forEach(function(value){
+            console.log(value)
+        })
+    })
+}
+
 https.get(url,function(res){
     var html=''
     res.on('data',function(data){
@@ -8,7 +41,10 @@ https.get(url,function(res){
     })
 
     res.on('end',function(){
-        console.log(html)
+        var result=filterMenu(html)
+        printMenu(result)
+        //console.log(result)
+
     })
 
     res.on('error',function(err){

e0d0c2dac73eebba78fd95a1affa34c9b6befb21
Soderberg Zuo <zuo.houde@gmail.com>
2018-10-15 00:11:10 +0800

    get request

--- Project06/modules/router.js
+++ Project06/modules/router.js
@@ -1,11 +1,17 @@
 var file=require('./file')
+var url=require('url')
 module.exports={
     home:function(req,res){
         //res.write('home page')
         file.readFile('./views/home.html',res,req)
     },
+
     login:function(req,res){
-        res.write('login page')
+        //res.write('login page')
+        let urlObject=url.parse(req.url,true).query
+        console.log(urlObject.email)
+        console.log(urlObject.password)
+        file.readFile('./views/login.html',res,req) 
     },
     register:function(req,res){
         res.write('register page')
--- Project06/router_2.js
+++ Project06/router_2.js
@@ -1,14 +1,15 @@
 const http=require('http')
-const url=require('url')
+var url=require('url')
 var router=require('./modules/router')
 http.createServer(function(req,res){
-    res.writeHead(200,{'content-Type':'image/jpeg'})
+    res.writeHead(200,{'content-Type':'text/html;charset=utf-8'})
     if(req.url!=='/favicon.ico'){
         var pathName=url.parse(req.url).pathname.replace(/\//,'');
         console.log(pathName)
         try{
             router[pathName](req,res)
         } catch(error){
+            console.log(error)
             router['home'](req,res)
         }
         

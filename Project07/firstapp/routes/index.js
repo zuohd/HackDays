bb4dce9db975d13ba8813278ffe3e041c8502f66
Soderberg Zuo <zuo.houde@gmail.com>
2018-10-16 15:20:45 +0800

    use ejs template

--- Project07/firstapp/routes/index.js
+++ Project07/firstapp/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: '<b>Express</b>',
+    isShow: true,
+    content: '<b>ok</b>'
+  });
 });
 
 module.exports = router;

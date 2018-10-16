0fd243b2d9423e53fccfea053db8a75cd26de303
Soderberg Zuo <zuo.houde@gmail.com>
2018-10-16 14:35:45 +0800

    route defination

--- Project07/firstapp/routes/users.js
+++ Project07/firstapp/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/list',function(req,res){
+  res.send('user list')
+})
+
+router.get('/ab*cd',function(req,res){
+  res.send('regexpression')
+})
+
 module.exports = router;

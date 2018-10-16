18cf0f26711f4f1e3404a3c2c8c022515f9a46dd
Soderberg Zuo <zuo.houde@gmail.com>
2018-10-16 14:56:13 +0800

    render file

--- Project07/firstapp/routes/users.js
+++ Project07/firstapp/routes/users.js
@@ -14,4 +14,15 @@ router.get('/ab*cd',function(req,res){
   res.send('regexpression')
 })
 
+router.get('/form',function(req,res){
+  res.sendFile(__dirname+'/form.html')
+})
+
+router.post('/save',function(req,res){
+  res.send('form submited')
+})
+/* Both handler for get and post */
+router.all('/all',function(req,res){
+  res.send('all')
+})
 module.exports = router;
